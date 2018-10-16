95900ef6d22e90082c15ddecfa9a55e2c94d6480
amy <a.gomez7@lab.matcom.uh.cu>
2018-10-16 11:54:22 -0400

    changes on reading and input generation for training data

--- data_reader.py
+++ data_reader.py
@@ -15,13 +15,13 @@ def sorted_file_names(path, set):
 #returns a tuple k where:
 #k[0] is a list containing all the images names
 #k[1] is a list with len(k[1]) == len(k[1]) containing all the images labels
-def training_data(path):
-    files = os.listdir('%s/labeled/%s' % (path, "train"))
+def training_data(path, d_set):
+    files = os.listdir('%s/labeled/%s' % (path, d_set))
     files.sort()
     return files, [i[0:4] for i in files]
 
-def training_labels(path):
-    files = os.listdir('%s/labeled/%s' % (path, "train"))
+def training_labels(path, d_set):
+    files = os.listdir('%s/labeled/%s' % (path, d_set))
     files.sort()
     return [i[0:4] for i in files]
 
@@ -34,8 +34,15 @@ def read_data(path, img_names, labels):
     return(np.array(images), np.array(labels))
 
 def get_training_set():
-    images_file = training_data(".")
+    images_file = training_data(".", "train")
     return read_data("./labeled/train/", images_file[0], images_file[1])
 
+def get_validation_set():
+    images_file = training_data(".", "val")
+    return read_data("./labeled/val/", images_file[0], images_file[1])
+
 def get_training_labels():
-    return training_labels(".")
+    return training_labels(".", "train")
+
+def get_validation_labels():
+    return training_labels(".", "val")
