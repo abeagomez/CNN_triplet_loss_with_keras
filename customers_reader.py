8c2462e448593629e6ff985979ad3f45d3ba569a
abeagomez <abeagomez@gmail.com>
2018-12-05 20:08:59 -0500

    continuous scores added

--- customers_reader.py
+++ customers_reader.py
@@ -58,7 +58,7 @@ def fill_all_images(lines_no, combinations, shuffled, weights, images_no):
             images_list.append(images[int(triplet[1])])
         scores_list = loading_weights.set_of_scores(weights, np.array(images_list))
         for i in range(len(scores_list)):
-            with open('combinations1_scores.txt', 'a') as file:
+            with open('continuous1_scores.txt', 'a') as file:
                 file.write(str(scores_list[i]) + " " + labels_list[i] + "\n")
         labels_list = []
         images_list = []
@@ -69,5 +69,5 @@ def fill_all_images(lines_no, combinations, shuffled, weights, images_no):
 #                        "triplet_loss_sigmoid_weights")
 
 
-fill_all_images(1119297, "combinations1.txt", "shuffled1.txt",
+fill_all_images(113115, "continuous1.txt", "list1.txt",
             "triplet_loss_sigmoid_weights", 113664)
